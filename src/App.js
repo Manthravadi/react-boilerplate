20ce0618ead7c879ce06b1485b5561d380317905
Manthravadi <m.harshavardhan@gmail.com>
2020-03-11 01:29:05 +0530

    Configured Prettier

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { hot } from "react-hot-loader";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { hot } from 'react-hot-loader'
 
-const Warning = React.lazy(() => import("./Warning"));
+const Warning = React.lazy(() => import('./Warning'))
 
 class App extends React.Component {
   state = {
     count: 0
-  };
+  }
 
   increment = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
+    this.setState({ count: this.state.count + 1 })
+  }
 
   decrement = () => {
-    this.setState({ count: this.state.count - 1 });
-  };
+    this.setState({ count: this.state.count - 1 })
+  }
 
   render() {
     //debugger;
-    var count = this.state.count;
+    var count = this.state.count
     return (
       <div>
         <h1>Hello World from the dev server!</h1>
-        <h2 className={this.state.count > 10 ? "warning" : null}>
+        <h2 className={this.state.count > 10 ? 'warning' : null}>
           Count: {this.state.count}
         </h2>
         <button
           onClick={() => {
-            this.increment();
+            this.increment()
           }}
         >
           +
         </button>
         <button
           onClick={() => {
-            this.decrement();
+            this.decrement()
           }}
         >
           -
@@ -46,8 +46,8 @@ class App extends React.Component {
           </React.Suspense>
         ) : null}
       </div>
-    );
+    )
   }
 }
 
-export default hot(module)(App);
+export default hot(module)(App)
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,19 +1,19 @@
-const merge = require("webpack-merge");
-const baseConfig = require("./webpack.config.base");
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.config.base')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 module.exports = merge(baseConfig, {
-  mode: "production",
+  mode: 'production',
   plugins: [
     new BundleAnalyzerPlugin({
       analyzerPort: 8000,
-      analyzerMode: "static",
+      analyzerMode: 'static',
       openAnalyzer: false,
-      reportFileName: "bundle_sizes.html"
+      reportFileName: 'bundle_sizes.html'
     })
   ],
   externals: {
-    react: "React",
-    "react-dom": "ReactDOM"
+    react: 'React',
+    'react-dom': 'ReactDOM'
   }
-});
+})
