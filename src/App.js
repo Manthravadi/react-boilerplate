f8369d5627be5c3766702b2d05792f46f0fbc274
Manthravadi <m.harshavardhan@gmail.com>
2020-03-11 00:16:27 +0530

    After test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import { hot } from "react-hot-loader";
 
-export default class App extends React.Component {
+const Warning = React.lazy(() => import("./Warning"));
+
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
   render() {
-    <div>Hello World!</div>;
+    //debugger;
+    var count = this.state.count;
+    return (
+      <div>
+        <h1>Hello World from the dev server!</h1>
+        <h2 className={this.state.count > 10 ? "warning" : null}>
+          Count: {this.state.count}
+        </h2>
+        <button
+          onClick={() => {
+            this.increment();
+          }}
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            this.decrement();
+          }}
+        >
+          -
+        </button>
+        {count > 10 ? (
+          <React.Suspense fallback={null}>
+            <Warning />
+          </React.Suspense>
+        ) : null}
+      </div>
+    );
   }
 }
+
+export default hot(module)(App);
--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-import getGreeting from "./greet";
-console.log(getGreeting("World"));
+import React from "react";
+import ReactDOM from "react-dom";
+import "@babel/polyfill";
+import App from "./App";
+import "./styles.css";
+
+ReactDOM.render(<App />, document.getElementById("app"));
