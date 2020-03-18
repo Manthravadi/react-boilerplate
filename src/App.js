4521408f9a7e514193943d87f0e3e38806046468
MANTHRAVADI Harsha <harsha.manthravadi@hexagon.com>
2020-03-18 11:15:15 +0530

    Final Code Cleanup

--- src/App.js
+++ src/App.js
@@ -1,49 +1,14 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 
-const Warning = React.lazy(() => import('./Warning'))
 class App extends React.Component {
-  state = {
-    count: 0
-  }
-
-  increment = () => {
-    this.setState({ count: this.state.count + 1 })
-  }
-
-  decrement = () => {
-    this.setState({ count: this.state.count - 1 })
-  }
-
   render() {
-    //debugger;
-    var count = this.state.count
     return (
-      <div>
-        <h1>Hello World from the dev server!</h1>
-        <h2 className={this.state.count > 10 ? 'warning' : null}>
-          Count: {this.state.count}
-        </h2>
-        <button
-          onClick={() => {
-            this.increment()
-          }}
-        >
-          +
-        </button>
-        <button
-          onClick={() => {
-            this.decrement()
-          }}
-        >
-          -
-        </button>
-        {count > 10 ? (
-          <React.Suspense fallback={null}>
-            <Warning />
-          </React.Suspense>
-        ) : null}
-      </div>
+      <main>
+        <div>
+          <h1>Hello World from the dev server!</h1>
+        </div>
+      </main>
     )
   }
 }
